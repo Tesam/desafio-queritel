28a0635d60b4aca0180b07f435ba7c9f14e81265
Tesam <mtsamudiog@gmail.com>
2021-02-16 18:31:27 -0300

    feat(cart): fetch data from cart_table to display on card

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,3 +1,7 @@
+import 'dart:typed_data';
+
+import 'package:desafio_queritel/utils/cart_table.dart';
+import 'package:desafio_queritel/utils/database_helper.dart';
 import 'package:flutter/material.dart';
 
 class Cart extends StatefulWidget {
@@ -6,95 +10,121 @@ class Cart extends StatefulWidget {
 }
 
 class _CartState extends State<Cart> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          _cart_card,
-          _cart_card,
-          _cart_card,
-        ],
-      )
-    );
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  String itemName = '', itemCategory = '';
+  double weight = 2.0, itemPrice = 1.0, itemQuantity = 3.0;
+  Uint8List imgUrl;
 
+  @override
+  void initState() {
+    super.initState();
+    loadDatabase();
   }
 
-  final Widget _cart_card = Container(
-      padding: EdgeInsets.fromLTRB(10,10,10,0),
-      height: 220,
-      width: double.maxFinite,
-      child: Card(
-        elevation: 5,
-        child:
-        Container(
-          padding: EdgeInsets.only(left: 8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                //    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image(
-                    image: AssetImage('assets/images/product.jpg'),
-                    height: 80.0,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+  void loadDatabase() async {
+    Future<CardTable> todoListFuture = databaseHelper.getInfo('ds');
+    todoListFuture.then((value) {
+      print('BASE DE DATOS: ${value.itemName}, ${value.itemCategory}, ${value.weight}, ${value.itemPrice}, ${value.itemQuantity}, ${value.imgUrl}');
+
+      setState(() {
+        this.itemName = value.itemName;
+        this.itemCategory = value.itemCategory;
+        this.weight = value.weight;
+        this.itemPrice = value.itemPrice;
+        this.itemQuantity = value.itemQuantity;
+        this.imgUrl = value.imgUrl;
+      });
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    if (imgUrl == null) {
+      return CircularProgressIndicator();
+    }
+
+    return Scaffold(
+        body: Column(
+        children: [
+          Container(
+              padding: EdgeInsets.fromLTRB(10,10,10,0),
+              height: 220,
+              width: double.maxFinite,
+              child: Card(
+                elevation: 5,
+                child:
+                Container(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      FittedBox(
-                        child: Text(
-                          "Item name",
-                          style: TextStyle(
-                            fontSize: 16.0,
+                      Row(
+                        children: [
+                          Image(
+                            image: MemoryImage(imgUrl),
+                            height: 80.0,
                           ),
-                        ),),
-                      FittedBox(child: Text(
-                        "Item category, weight, price",
-                        style: TextStyle(
-                            fontSize: 14.0
-                        ),
-                      ),),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              FittedBox(
+                                child: Text(
+                                  this.itemName.toString(),
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                  ),
+                                ),),
+                              FittedBox(child: Text(
+                                '${this.itemCategory.toString()}, ${this.weight.toString()}, ${this.itemPrice.toString()}',
+                                style: TextStyle(
+                                    fontSize: 14.0
+                                ),
+                              ),),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          InkWell(
+                            //onTap:
+                            child: Container(
+                              height: 40.0,
+                              width: 100.0,
+                              margin: EdgeInsets.only(left: 8.0, right: 8.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30.0),
+                                color: Colors.blueAccent,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Remove",
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      color: Colors.white
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          FittedBox(
+                            child: Text(
+                              this.itemQuantity.toString(),
+                              style: TextStyle(
+                                fontSize: 18.0,
+                              ),
+                            ),),
+                        ],
+                      ),
                     ],
                   ),
-                ],
-              ),
-              Row(
-                children: [
-                  InkWell(
-                    //onTap:
-                    child: Container(
-                      height: 40.0,
-                      width: 100.0,
-                      margin: EdgeInsets.only(left: 8.0, right: 8.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30.0),
-                        color: Colors.blueAccent,
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Remove",
-                          style: TextStyle(
-                              fontSize: 18.0,
-                              color: Colors.white
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  FittedBox(
-                    child: Text(
-                      "Quantity",
-                      style: TextStyle(
-                        fontSize: 18.0,
-                      ),
-                    ),),
-                ],
-              ),
-            ],
-          ),
-        ),
-      )
-  );
+                ),
+              )
+          )
+      ],
+    ));
+  }
+
 }
---
+++ lib/utils/cart_table.dart
@@ -0,0 +1,72 @@
+import 'dart:typed_data';
+
+class CardTable {
+  String _itemName, _itemCategory, _itemBrand, _weightLabel, _state;
+  int _id;
+  double _itemPrice, _itemQuantity, _weight;
+  Uint8List _imgUrl;
+
+  CardTable(
+      this._itemName,
+      this._itemCategory,
+      this._itemBrand,
+      this._weightLabel,
+      this._state,
+      this._id,
+      this._itemPrice,
+      this._itemQuantity,
+      this._weight,
+      this._imgUrl,
+      );
+
+  String get itemName =>_itemName;
+  String get itemCategory =>_itemCategory;
+  String get itemBrand =>_itemBrand;
+  String get weightLabel =>_weightLabel;
+  String get state =>_state;
+
+  int get id =>_id;
+
+  double get itemPrice =>_itemPrice;
+  double get itemQuantity =>_itemQuantity;
+  double get weight =>_weight;
+
+  Uint8List get imgUrl =>_imgUrl;
+
+  // Convert a Note object into a Map object
+  Map<String, dynamic> toMap() {
+
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = _id;
+    }
+
+    map['item_name'] = _itemName;
+    map['item_category'] = _itemCategory;
+    map['item_brand'] = _itemBrand;
+    map['weight_label'] = _weightLabel;
+    map['state'] = _state;
+    map['item_price'] = _itemPrice;
+    map['item_quantity'] = _itemQuantity;
+    map['weight'] = _weight;
+    map['img_url'] = _imgUrl;
+
+    return map;
+  }
+
+  // Extract a Note object from a Map object
+  CardTable.fromMapObject(Map<String, dynamic> map) {
+
+    this._id = map['id'];
+    this._itemName = map['item_name'];
+    this._itemCategory = map['item_category'];
+    this._itemBrand = map['item_brand'];
+    this._weightLabel = map['weight_label'];
+    this._state = map['state'];
+    this._itemPrice = map['item_price'];
+    this._itemQuantity = map['item_quantity'];
+    this._weight = map['weight'];
+    this._imgUrl = map['img_url'];
+
+  }
+}
