fef49695e2e6b458d6e112e3c62c4675308360fd
TesslaRay <cristian.valdivia@alumnos.usm.cl>
2021-02-17 16:47:24 -0300

    feature: listview with order card

---
+++ lib/components/order_card.dart
@@ -0,0 +1,70 @@
+import 'package:desafio_queritel/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class OrderCard extends StatelessWidget {
+  final String orderId;
+  final double price;
+  final String date;
+
+  const OrderCard({Key key, this.orderId, this.date, this.price})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        height: 170,
+        width: MediaQuery.of(context).size.width * 0.9,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(24),
+          ),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Center(
+                  child: Text(
+                'Order: ' + orderId,
+                style: TextStyle(
+                  color: PRIMARY,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800,
+                ),
+              )),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text('Price: ' + price.toString() + ' USD')),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 10),
+              child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text('Date: ' + date)),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                  child: Text(
+                'Details',
+                style: TextStyle(
+                  color: TEXT,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800,
+                ),
+              )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/orders.dart
+++ lib/screens/orders.dart
@@ -1,58 +1,91 @@
+import 'package:desafio_queritel/components/order_card.dart';
+import 'package:desafio_queritel/db/database_helper.dart';
+import 'package:desafio_queritel/models/order_table.dart';
 import 'package:desafio_queritel/utils/colors.dart';
 import 'package:flutter/material.dart';
 
-class Orders extends StatelessWidget {
+class Orders extends StatefulWidget {
+  @override
+  _OrdersState createState() => _OrdersState();
+}
+
+class _OrdersState extends State<Orders> {
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  Future<List<OrderTable>> promiseOrderList;
+  List<OrderTable> orderList;
+
+  @override
+  void initState() {
+    super.initState();
+    promiseOrderList = databaseHelper.getOrders();
+    promiseOrderList.then(
+      (value) => setState(() {
+        orderList = value;
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: TEXT,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Align(
-              alignment: Alignment.centerLeft,
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color(0xffE5E5E5),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'My orders',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 2,
+                    color: TEXT,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
               child: Text(
-                'My orders',
+                'Find here your current and past orders',
                 style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
                   letterSpacing: 2,
                   color: TEXT,
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Find here your current and past orders',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w300,
-                letterSpacing: 2,
-                color: TEXT,
-              ),
+            Divider(),
+            Expanded(
+              child: orderList == null
+                  ? CircularProgressIndicator()
+                  : Container(
+                      child: ListView.builder(
+                        itemCount: orderList.length,
+                        itemBuilder: (_, index) {
+                          return OrderCard(
+                            orderId: orderList[index].id.toString(),
+                            price: orderList[index].price,
+                            date: orderList[index].date,
+                          );
+                        },
+                      ),
+                    ),
             ),
-          ),
-          Divider(),
-          Container(
-            height: 150,
-            width: MediaQuery.of(context).size.width * 0.7,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(
-                Radius.circular(24),
-              ),
-            ),
-            child: Center(child: Text('wena')),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
